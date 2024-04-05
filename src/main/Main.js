f2acb583f994310a03a72df070ea41260ccb5950
Jasur0919 <jasurjohn1111@gmail.com>
2024-04-05 22:05:44 +0500

    fgbtnfdsAB

--- src/main/Main.js
+++ src/main/Main.js
@@ -84,52 +84,52 @@ function Main() {
     id: 1,
     title: Product ,
     url: ProductPrice,
-    heart: <ImHeartBroken />,
-    eye: <CgEye />
+    heart: <ImHeartBroken className='product_heart'/>,
+    eye: <CgEye  className='product_heart'/>
 
   },
   {
     id: 1,
     title: Product2,
     url: Price2,
-    heart: <ImHeartBroken />,
-    eye: <CgEye />
+    heart: <ImHeartBroken className='product_heart'/>,
+    eye: <CgEye  className='product_heart'/>
     
   },
   {
     id: 1,
     title: Product3,
     url: Price3,
-    heart: <ImHeartBroken />,
-    eye: <CgEye />
+    heart: <ImHeartBroken className='product_heart'/>,
+    eye: <CgEye  className='product_heart'/>
   },
   {
     id: 1,
     title: Product5,
     url: Price5,
-    heart: <ImHeartBroken />,
-    eye: <CgEye />
+    heart: <ImHeartBroken className='product_heart'/>,
+    eye: <CgEye  className='product_heart'/>
   },
   {
     id: 1,
     title: Product5,
     url: Price5,
-    heart: <ImHeartBroken />,
-    eye: <CgEye />
+    heart: <ImHeartBroken className='product_heart'/>,
+    eye: <CgEye  className='product_heart'/>
   },
   {
     id: 1,
     title: Product6,
     url: Price6,
-    heart: <ImHeartBroken />,
-    eye: <CgEye />
+    heart: <ImHeartBroken className='product_heart'/>,
+    eye: <CgEye  className='product_heart'/>
   },
   {
     id: 1,
     title: Product7,
     url: Price7,
-    heart: <ImHeartBroken />,
-    eye: <CgEye />
+    heart: <ImHeartBroken className='product_heart'/>,
+    eye: <CgEye  className='product_heart'/>
   },
   {
     id: 1,
@@ -147,6 +147,7 @@ function Main() {
                     {el.heart}
                     {el.eye}
 
+
                 {/* <svg width={35} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M119.4 44.1c23.3-3.9 46.8-1.9 68.6 5.3l49.8 77.5-75.4 75.4c-1.5 1.5-2.4 3.6-2.3 5.8s1 4.2 2.6 5.7l112 104c2.9 2.7 7.4 2.9 10.5 .3s3.8-7 1.7-10.4l-60.4-98.1 90.7-75.6c2.6-2.1 3.5-5.7 2.4-8.8L296.8 61.8c28.5-16.7 62.4-23.2 95.7-17.6C461.5 55.6 512 115.2 512 185.1v5.8c0 41.5-17.2 81.2-47.6 109.5L283.7 469.1c-7.5 7-17.4 10.9-27.7 10.9s-20.2-3.9-27.7-10.9L47.6 300.4C17.2 272.1 0 232.4 0 190.9v-5.8c0-69.9 50.5-129.5 119.4-141z"/></svg>
                  
 
@@ -156,6 +157,8 @@ function Main() {
                 </div>
                 <h3>Breed Dry Dog Food</h3>
                 <img src={el.url} alt="" />
+
+                <button className='product_btn'>Add To Cart</button>
               </div> 
 
               )
